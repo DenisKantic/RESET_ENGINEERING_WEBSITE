d4b497021022aea09dae2833c4042c88af4dae8d
DenisKantic <denis.kantic18@gmail.com>
2023-03-05 19:11:03 +0100

    finished structure for homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,8 +3,15 @@ import React from 'react'
 const HomePage = () => {
   return (
     <div className='w-full h-[100vh]'>
-        <div className='mx-auto w-[90%] bg-green-400 h-[100vh]'>
-            <h1 className='text-3xl'>RESET INŽINJERING</h1>
+        <div className='mx-auto w-[90%] h-[100vh] flex items-center justify-start h-screen'>
+            <div className='w-[50%] h-[50vh] p-5 z-10 flex items-start justify-center flex-col'>
+            <h1 className='text-[70px]'>RESET <br /> INŽINJERING</h1>
+            <p className='text-2xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, adipisci!</p>
+            <div className='flex flex-row mt-4'>
+                <button className='bg-orange-400 mr-4 rounded-xl p-2 text-xl'>Ko smo mi?</button>
+                <button className='bg-orange-400 p-2 rounded-xl text-xl'>Nase usluge</button>
+            </div>
+            </div>
         </div>
     </div>
   )
