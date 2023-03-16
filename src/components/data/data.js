9150d9c01d97e426dc41b824acb615bde24d7193
DenisKantic <denis.kantic18@gmail.com>
2023-03-16 16:54:27 +0100

    finished the products components, swiper for the next component is still in progress. Some small fix on some components are made.

---
+++ src/components/data/data.js
@@ -0,0 +1,20 @@
+export const data = [
+    {
+        id: 1,
+        image: `https://helpx.adobe.com/content/dam/help/en/indesign/how-to/business-card-design/jcr_content/main-pars/image_1047841121/business-card-design-hero_900x506.jpg.img.jpg`, 
+        header: "test1",
+        price: "cijena1"
+    },
+    {
+        id: 2,
+        image: `https://cms.cloudinary.vpsvc.com/images/c_scale,dpr_auto,f_auto,q_auto:best,t_productPageHeroGalleryTransformation,w_auto/legacy_dam/en-us/S001325521/marquee-business-card-standard-001?cb=36a0ecb9eb037fc73590eeb26411458109912692`,   
+        header: "test2",
+        price: "cijena2"
+    },
+    {
+        id: 3,
+        image: `https://helpx.adobe.com/content/dam/help/en/indesign/how-to/business-card-design/jcr_content/main-pars/image_1047841121/business-card-design-hero_900x506.jpg.img.jpg`, 
+        header: "test3",
+        price: "cijena3"
+    }
+];
