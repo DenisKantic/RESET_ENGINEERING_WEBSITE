0b9f76f990161d6decec869396f22fe9db4751be
DenisKantic <denis.kantic18@gmail.com>
2023-03-18 20:41:36 +0100

    created components for sweiper carousell

--- src/components/OtherServices.jsx
+++ src/components/OtherServices.jsx
@@ -1,37 +1,35 @@
 import React from 'react'
 import {data} from './data/data.js';
 import {Swiper, SwiperSlide} from 'swiper/react';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Navigation, Pagination, A11y } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
 
 const OtherServices = () => {
   return (
-    <div className='w-full h-screen bg-[#f1f5f9] bg-red-400'>
+    <div className='w-full h-screen bg-[#f1f5f9]'>
       <h1 className='text-center text-4xl p-10'>Nase ostale usluge</h1>
 
-      {data.map(data =>{
-      
-      return  <Swiper slidesPerView={3} modules={[Navigation, Pagination, Scrollbar, A11y]} 
-      navigation
-      pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
-      spaceBetween={50}
-      className='w-full h-screen'>
-       <SwiperSlide key={data.id}>
-        <div className='w-full h-[80vh] mt-10 bg-blue-400 bg-cover bg-no-repeat' style={{backgroundImage: `url(${data.image})`}}>
-            <div className='mx-auto w-full h-[80vh] flex items-start justify-center flex-col p-4'>
-              <h1 className='text-4xl'>{data.header}</h1> 
-              <p className='text-2xl'>{data.price}</p>
-            </div>
-        </div>
-        </SwiperSlide>
-      </Swiper>
+    <Swiper 
+    modules={[Navigation, Pagination, A11y]}
+    spaceBetween={50}
+    slidesPerView={1}
+    navigation
+    pagination={{ clickable: true }}
+    scrollbar={{ draggable: true }}
+    className='h-[80vh] w-full'>
+      {data.map(item =>{
+        console.log(item.image)
+        return <SwiperSlide key={item.id} 
+        className="w-full bg-cover bg-center h-screen" style={{backgroundImage: `url(${item.image})`}}>
+          <div>
+          <h1>{item.header}</h1>
+          <h1>{item.price}</h1>
+          </div>
+          </SwiperSlide>
       })}
+    </Swiper>
       </div>
   )
 }
