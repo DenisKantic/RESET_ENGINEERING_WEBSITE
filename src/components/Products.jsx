3b56df8a0cbbb45cceb39b54da9cbfdc4cda53d0
DenisKantic <denis.kantic18@gmail.com>
2023-03-22 12:05:41 +0100

    fix for mobile responsive

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -23,55 +23,65 @@ const Products = () => {
           </div>
 
           <div>
-          <div className='w-[400px] h-[300px] p-8 rounded-t-xl' id='vizitke' >
+          <div className='w-[400px] h-[300px] p-8 rounded-t-xl
+            xss:w-full md:w-[400px]' id='vizitke' >
           </div>
            
-           <div className='w-[400px] h-auto bg-[#e2e6ea] p-4 rounded-b-xl'>
-           <h1 className='text-2xl'>Vizit kartice</h1>
+           <div className='w-[400px] h-auto bg-[#e2e6ea] p-4 rounded-b-xl
+            xss:w-full mb-10 md:w-[400px]'>
+           <h1 className='text-2xl p-1 bg-gray-400 w-[50%] xss:w-[60%] md:w-[50%]'>Vizit kartice</h1>
             <p className='text-md mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, vitae.</p>
            </div>
           </div>
           
           <div>
-          <div className='w-[400px] h-[300px] p-8 rounded-t-xl' id='solje'>
+          <div className='w-[400px] h-[300px] p-8 rounded-t-xl
+              xss:w-full md:w-[400px]' id='solje'>
           </div>
            
-           <div className='w-[400px] h-auto bg-[#e2e6ea] rounded-b-xl p-4'>
-           <h1 className='text-2xl'>Personalne solje</h1>
+           <div className='w-[400px] h-auto bg-[#e2e6ea] rounded-b-xl p-4
+                 xss:w-full mb-10 md:w-[400px] '>
+           <h1 className='text-2xl p-1 bg-gray-400 w-[50%] xss:w-[60%] md:w-[50%]'>Personalne solje</h1>
             <p className='text-md mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, vitae.</p>
            </div>
           </div>
           
           </div>
 
-          <div className='flex justify-between mt-[100px] xss:flex-col  md:flex-row'>
+          <div className='flex justify-between  xss:flex-col  md:flex-row'>
            {/*container*/}
            <div >
-          <div className='w-[400px] h-[300px] p-8 rounded-t-xl' id='naljepnice'>
+          <div className='w-[400px] h-[300px] p-8 rounded-t-xl
+          xss:w-full md:w-[400px]' id='naljepnice'>
           </div>
            
-           <div className='w-[400px] h-auto bg-[#e2e6ea] rounded-b-xl p-4'>
-           <h1 className='text-2xl'>PVC naljepnice</h1>
+           <div className='w-[400px] h-auto bg-[#e2e6ea] rounded-b-xl p-4
+           xss:w-full mb-10 md:w-[400px]'>
+           <h1 className='text-2xl p-1 bg-gray-400 w-[50%] xss:w-[60%] md:w-[50%]'>PVC naljepnice</h1>
             <p className='text-md mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, vitae.</p>
            </div>
           </div>
 
           <div>
-          <div className='w-[400px] h-[300px] p-8 rounded-t-xl' id='letak'>
+          <div className='w-[400px] h-[300px] p-8 rounded-t-xl
+          xss:w-full md:w-[400px]' id='letak'>
           </div>
            
-           <div className='w-[400px] h-auto bg-[#e2e6ea] rounded-b-xl p-4'>
-           <h1 className='text-2xl'>Letak</h1>
+           <div className='w-[400px] h-auto bg-[#e2e6ea] rounded-b-xl p-4
+           xss:w-full mb-10 md:w-[400px]'>
+           <h1 className='text-2xl p-1 bg-gray-400 w-[50%] xss:w-[60%] md:w-[50%]'>Letak</h1>
             <p className='text-md mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, vitae.</p>
            </div>
           </div>
           
           <div>
-          <div className='w-[400px] h-[300px] p-8 rounded-t-xl' id='privjesak'>
+          <div className='w-[400px] h-[300px] p-8 rounded-t-xl
+          xss:w-full md:w-[400px]' id='privjesak'>
           </div>
            
-           <div className='w-[400px] h-auto rounded-b-xl p-4 bg-[#e2e6ea]'>
-           <h1 className='text-2xl'>Privjesci</h1>
+           <div className='w-[400px] h-auto rounded-b-xl p-4 bg-[#e2e6ea]
+           xss:w-full mb-10 md:w-[400px]'>
+           <h1 className='text-2xl p-1 bg-gray-400 w-[50%] xss:w-[60%] md:w-[50%]'>Privjesci</h1>
             <p className='text-md mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, vitae.</p>
            </div>
           </div>
