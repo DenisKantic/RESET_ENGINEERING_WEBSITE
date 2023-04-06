07388e675e527824537f152bafa40027fa00dfd4
DenisKantic <denis.kantic18@gmail.com>
2023-04-06 19:28:55 +0200

    fixed small things in aboutUs and added some text

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -9,7 +9,7 @@ const AboutUs = () => {
                         xss:w-[95%] md:w-[90%]'>
       
         {/* left side */}
-        <div className='w-[40%] h-[35vh] 
+        <div className='w-[40%] h-[35vh] mt-20
                         xss:hidden xl:block' id='people'>
          
         </div>
@@ -29,26 +29,29 @@ const AboutUs = () => {
                           xss:w-full'>
             <h1 className='xss:text-2xl lg:text-3xl'>Ko smo mi?</h1>
             <p className='mt-3 text-left 
-                          xss:text-md font-[300]'>RE-SET INŽINJERING je kompanija koja zapošljava isključivo osobe sa invaliditetom, kako bi živjele što lagodniji život</p>
+                          xss:text-md font-[300]'>RESET INŽINJERING je kompanija koja zapošljava isključivo osobe sa invaliditetom. Osnivač kompanije je nevladino udruženje RE-SET
+                          koja okuplja mlade invalidne osobe na području TK. </p>
           </div>
           <div className='w-[80%] p-3 
                           xss:w-full'>
             <h1 className='xss:text-2xl lg:text-3xl'>Naša misija?</h1>
             <p className='mt-3 text-left 
-                          xss:text-md font-[300]'>Pružiti priliku mladim osobama sa invaliditetom da pridonose zajednici i da se osjećaju korisnim. </p>
+                          xss:text-md font-[300]'>Pružiti priliku mladim osobama sa invaliditetom da pridonose zajednici i da se osjećaju korisnim. Njihovim aktivnim uključenjem u zajednicu, omogućavamo
+                          da pokažu svoj potencijal.</p>
           </div>
         
           <div className='w-[80%] p-3 
                           xss:w-full'>
             <h1 className='xss:text-2xl lg:text-3xl'>Naše usluge?</h1>
             <p className='mt-3 text-left 
-                          xss:text-md font-[300]'>Nudimo razne usluge kao što su: izrade vizit kartica, privjesaka, printa na majice, šolje, personalizirane čestitke i mnoštvo toga.</p>
+                          xss:text-md font-[300]'>Nudimo razne usluge kao što su: grafički dizajn, izrade vizit kartica, privjesaka, printa na majice, šolje, personalizirane čestitke i mnoštvo toga.</p>
           </div>
           <div className='w-[80%] p-3 
                           xss:w-full '>
             <h1 className='xss:text-2xl lg:text-3xl'>Zašto nas izabrati?</h1>
             <p className='mt-3 text-left 
-                          xss:text-md font-[300]'>Zato jer nudimo najbolje cijene i najbolju kvalitetu na cijelom tržištu. Ujedno podržavate i udruženje "RE-SET", koje je i osnovalo ovu kompaniju.</p>
+                          xss:text-md font-[300]'>Zato što nudimo najbolji odnos cijene i kvalitete proizvoda na tržištu. Kupovinom naših proizvoda, direktno podržavate udruženje
+                          "RE-SET" i omogućavate više prilika za mlade invalidne osobe.</p>
           </div>
         </div>
         
