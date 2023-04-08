0549cf8d0f6380da85f69351260da97ebe6e0a8e
DenisKantic <denis.kantic18@gmail.com>
2023-04-08 11:53:03 +0200

    finishing contact page, pushing to keep the original layout for improving

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,9 +3,17 @@ import {AiOutlineHome,AiOutlinePhone,AiOutlineMail,AiOutlineMobile} from 'react-
 
 const Contact = () => {
   return (
-    <div className='w-full min-h-[70vh] bg-red-400 overflow-hidden' id="contact">
+    <div className='w-full min-h-[70vh] overflow-hidden' id="contact">
       <h1 className='text-4xl text-center p-5'>Kontaktirajte nas!</h1>
-    <div className='flex w-[70%] justify-between mx-auto items-center mt-[100px]'>
+
+      {/* wrapper */}
+      <div className='w-[90%] mx-auto'>
+      <iframe width="100%" className='h-[60vh] mt-10' frameborder="0"  marginheight="0" marginwidth="0" id="gmap_canvas" 
+      src="https://maps.google.com/maps?width=949&amp;height=501&amp;hl=en&amp;q=Franje%20markovi%C4%87a%2012%20Tuzla+(RESET%20IN%C5%BDINJERING)&amp;t=&amp;z=17&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
+      </iframe>
+
+
+    <div className='mx-auto bg-blue-400 flex justify-between p-5'>
       {/* item1 */}
       <div className='flex flex-col justify-center items-center'>
       <AiOutlineHome size={40}/>
@@ -33,9 +41,9 @@ const Contact = () => {
       <p>reset.inzinjering@gmail.com</p>
       </div>
 
-
       </div>
 
+      </div>
       </div>
   )
 }
