bcf80b9fcb13ff2af862932ea9f6a3381738cac0
DenisKantic <denis.kantic18@gmail.com>
2023-04-10 12:58:30 +0200

    trying to commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className='w-full flex justify-center items-center bg-[#354a67] text-white
     xss:flex-col h-[9vh] text-sm md:flex-row md:h-[4vh] md:text-lg'>
         <h1 className='cursor-default'>RESET INŽINJERING &copy; 2023</h1> 
-        <p className='xss:m-0 md:ml-5 cursor-default'>Made by: <a  className='font-bold cursor-pointer' href="https://www.linkedin.com/in/denis-kantic">Denis Kantic</a></p>
+        <p className='xss:m-0 md:ml-5 cursor-default'>Made by: <a className='font-bold cursor-pointer' href="https://www.linkedin.com/in/denis-kantic">Denis Kantic</a></p>
         </div>
   )
 }
